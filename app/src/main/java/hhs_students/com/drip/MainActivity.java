a85c010603363f22939b450a5bdeeb408439072e
hhs-rm51 <hhs-rm51@husd.com>
2016-09-16 09:54:39 -0700

    Initial commit

---
+++ app/src/main/java/hhs_students/com/drip/MainActivity.java
@@ -0,0 +1,164 @@
+package hhs_students.com.drip;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView stateAverage;
+    private String DEBUG_TAG = "ERROR:";
+    private String mQuery;
+    private int lineCount = 498;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        stateAverage = (TextView) findViewById(R.id.StateAverage);
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doMySearch(query);
+        }
+        String stringUrl = "http://cdec.water.ca.gov/cgi-progs/reservoirs/STORSUM";
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            new DownloadWebpageTask().execute(stringUrl);
+        } else {
+            TextView errorMSG = (TextView) findViewById(R.id.error_message);
+            errorMSG.setText(getString(R.string.network_connection_error));
+        }
+
+    }
+
+    private void doMySearch(String query) {
+        Intent intent = new Intent(this, SearchResult.class);
+        intent.putExtra("M_QUERY", query);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // action with ID action_refresh was selected
+            case R.id.action_search:
+                Toast.makeText(this, "Search", Toast.LENGTH_SHORT)
+                        .show();
+                break;
+            // action with ID action_settings was selected
+            case R.id.action_settings:
+                Toast.makeText(this, "Settings selected", Toast.LENGTH_SHORT)
+                        .show();
+                break;
+            default:
+                break;
+        }
+
+        return true;
+    }
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            stateAverage.setText(result);
+        }
+    }
+        private String downloadUrl(String myurl) throws IOException {
+            InputStream is = null;
+            // Only display the first 500 characters of the retrieved
+            // web page content.
+            int len = 500;
+
+            try {
+                URL url = new URL(myurl);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                // Starts the query
+                conn.connect();
+                int response = conn.getResponseCode();
+                Log.d(DEBUG_TAG, "The response is: " + response);
+                is = conn.getInputStream();
+                // Convert the InputStream into a string
+                String contentAsString = readIt(is, len);
+                return contentAsString;
+
+                // Makes sure that the InputStream is closed after the app is
+                // finished using it.
+            } finally {
+                if (is != null) {
+                    is.close();
+                }
+            }
+        }
+    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+        Reader reader;
+        reader = new InputStreamReader(stream, "UTF-8");
+        BufferedReader in = new BufferedReader(reader);
+
+        String temp;
+        int counter = 0;
+        String line = "";
+        temp = in.readLine();
+        while(temp != null) {
+            if(counter == lineCount) {
+                line = in.readLine();
+            }
+            temp = in.readLine();
+            counter++;
+        }
+        String[] TotalNums = line.split("</td><td align=right>");
+        return TotalNums[TotalNums.length-2] + "%";
+    }
+}
---
+++ app/src/main/java/hhs_students/com/drip/SearchResult.java
@@ -0,0 +1,158 @@
+package hhs_students.com.drip;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class SearchResult extends AppCompatActivity {
+    private static final String DEBUG_TAG = "HttpExample";
+    private TableLayout displayData;
+    private Button testButton;
+    private String mSearchReservoir;
+    private String endData;
+    private String[] mDataSplit;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_result);
+        testButton = (Button) findViewById(R.id.test_button);
+        displayData = (TableLayout) findViewById(R.id.data_table);
+        Intent intent = getIntent();
+        mSearchReservoir = intent.getStringExtra("M_QUERY");
+
+    }
+
+    // When user clicks button, calls AsyncTask.
+    // Before attempting to fetch the URL, makes sure that there is a network connection.
+    public void myClickHandler(View view) {
+        // Gets the URL from the UI's text field.
+        String stringUrl = "http://cdec.water.ca.gov/cgi-progs/queryCSV?station_id=kes&sensor_num=15&dur_code=D&start_date=&end_date=&data_wish=View+CSV+Data";//urlText.getText().toString();
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            new DownloadWebpageTask().execute(stringUrl);
+        } else {
+            TextView errorMSG = (TextView) findViewById(R.id.error_message);
+            errorMSG.setText(getString(R.string.network_connection_error));
+        }
+    }
+
+    // Uses AsyncTask to create a task away from the main UI thread. This task takes a
+    // URL string and uses it to create an HttpUrlConnection. Once the connection
+    // has been established, the AsyncTask downloads the contents of the webpage as
+    // an InputStream. Finally, the InputStream is converted into a string, which is
+    // displayed in the UI by the AsyncTask's onPostExecute method.
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            mDataSplit = result.split(";");
+            String[] temp;
+            for(int i = 2; i < mDataSplit.length; i++) {
+                temp = mDataSplit[i].split(",");
+                addRow(dateFormat(temp[0]), temp[2]);
+            }
+        }
+    }
+    private String downloadUrl(String myurl) throws IOException {
+        InputStream is = null;
+        // Only display the first 1000 characters of the retrieved
+        // web page content.
+        int len = 1000;
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d(DEBUG_TAG, "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is, len);
+            mDataSplit = contentAsString.split(";");
+            return contentAsString;
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+    public String readIt(InputStream stream, int len) throws IOException {
+        Reader reader;
+        reader = new InputStreamReader(stream, "UTF-8");
+        BufferedReader in = new BufferedReader(reader);
+
+        String line;
+        String data = "";
+        line = in.readLine();
+        while(line != null) {
+            data += line + ";";
+            line = in.readLine();
+        }
+        data = data.substring(0, data.length()-1);
+        return data;
+    }
+
+    public String dateFormat(String date){
+        return date.substring(4, 6) + "/" + date.substring(6) + "/" + date.substring(0,4);
+    }
+    public void addRow(String date, String levels) {
+        TableRow row= new TableRow(this);
+        TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+        row.setLayoutParams(lp);
+        TextView tDate = new TextView(this);
+        TextView tLevels = new TextView(this);
+        tDate.setText(date);
+        tDate.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT));
+        tLevels.setText(levels);
+        tDate.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+        row.addView(tDate);
+        row.addView(tLevels);
+        displayData.addView(row, new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
+    }
+}
